ab5152bf6646688a85ac1a0a7cea16bc71dda58f
Milan Jovanovic <jov.milan@elfak.rs>
2020-03-11 00:56:31 +0100

    functional links

---
+++ Test Suites/Functional Links.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Functional Links</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fcbfdd8f-4c13-43f7-b544-2cc71401561d</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ef78bdd-f01f-4bbe-aec9-b0c54ab4ee09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Functional Links/Shop Page Links</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
